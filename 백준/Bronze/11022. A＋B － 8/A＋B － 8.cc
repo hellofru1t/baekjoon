86ccc8713c7b3ea5d181669c5cf2062c9afd046c
hellofru1t <114278007+hellofru1t@users.noreply.github.com>
2022-09-24 17:04:02 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ldb;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef tuple<int, int, int> tiii;
+#define FASTIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define TEST int tt; cin >> tt; while(tt--) solve();
+#define MAXN 200005
+ll MOD = 1000000007;
+ll MOD2 = 998244353;
+int INF = 0x3f3f3f3f;
+
+
+// n(n+1)/2 시그마
+
+void solve(){
+    int i,j,k;
+    int n; cin >> n;
+    for(i=1; i<=n; i++){
+        int a,b; cin >> a >> b;
+        cout << "Case #" << i << ": " << a << " + " << b << " = " << a+b << '\n'; 
+    }
+}
+
+int main(){
+    FASTIO
+    solve();
+
+}
