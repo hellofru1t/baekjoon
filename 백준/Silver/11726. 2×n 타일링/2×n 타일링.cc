c3ded018a8e771cfdd10051673262a7f07ee8cc0
hellofru1t <114278007+hellofru1t@users.noreply.github.com>
2022-09-24 22:14:34 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ldb;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef tuple<int, int, int> tiii;
+#define FASTIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define TEST int tt; cin >> tt; while(tt--) solve();
+
+
+int DP[1001];
+int MOD = 10007;
+
+void solve() {
+    int i,j,k;
+    int n;
+    cin >> n;
+    DP[1] = 1;
+    DP[2] = 2;
+    for(i=3; i<=n; i++){
+        DP[i] = (DP[i-1] + DP[i-2])  % MOD;
+    }
+    cout << DP[n];
+}
+
+int main(){
+    FASTIO
+    solve();
+}
