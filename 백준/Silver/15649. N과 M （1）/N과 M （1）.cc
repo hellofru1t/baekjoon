ed67c05ad7db8df3963458bb52866284f1469095
hellofru1t <114278007+hellofru1t@users.noreply.github.com>
2022-09-24 22:20:05 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ldb;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef tuple<int, int, int> tiii;
+#define FASTIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define TEST int tt; cin >> tt; while(tt--) solve();
+
+
+int n,m;
+int arr[10];
+bool visited[10];
+
+void dfs(int cnt){
+    if(cnt == m){
+        for(int i = 0 ; i<m; i++){
+            cout << arr[i] << ' ';
+        } cout << '\n'; return;
+    }
+    for(int i = 1; i<= n; i++){
+        if(!visited[i]){
+            visited[i] = true;
+            arr[cnt] = i;
+            dfs(cnt+1);
+            visited[i] = false;
+        }
+    }
+}
+void solve(){
+    int i,j,k;
+    cin >> n >> m;
+    dfs(0  );
+}
+
+
+int main(){
+    FASTIO
+    solve();
+}
