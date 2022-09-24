a2d077320c1913a9d635e7ceebb528d3a66cb906
hellofru1t <114278007+hellofru1t@users.noreply.github.com>
2022-09-24 17:03:13 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ldb;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef tuple<int, int, int> tiii;
+#define FASTIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define TEST int tt; cin >> tt; while(tt--) solve();
+#define MAXN 200005
+ll MOD = 1000000007;
+ll MOD2 = 998244353;
+int INF = 0x3f3f3f3f;
+
+
+
+void solve(){
+    int i,j,k;
+    int a,b;
+
+    cout << "         ,r'\"7\n"
+            "r`-_   ,'  ,/\n"
+            " \\. \". L_r'\n"
+            "   `~\\/\n"
+            "      |\n"
+            "      |"; 
+}
+
+int main(){
+    FASTIO
+    solve();
+}
