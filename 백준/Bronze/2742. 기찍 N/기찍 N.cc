3ba3e3d4e857946677bcd9d8c7821b33bc1d7f0a
hellofru1t <114278007+hellofru1t@users.noreply.github.com>
2022-10-02 23:45:41 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ldb;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+#define FASTIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define TEST int tt; cin >> tt; while(tt--) solve();
+
+void solve(){
+    int i,j,k;
+    int n; cin >> n;
+    for(i=n; i; i--) cout << i << '\n';
+}
+int main() {
+    FASTIO
+    solve(); 
+}
