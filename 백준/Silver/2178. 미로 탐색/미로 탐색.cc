d4a1cd074931a8fd7396d449faf3d0c596d3a9d3
hellofru1t <114278007+hellofru1t@users.noreply.github.com>
2022-10-06 12:00:41 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2112 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -1,35 +1,28 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 typedef long long ll;
-typedef long double ldb;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef pair<double, double> pdd;
-typedef tuple<int, int, int> tiii;
-#define FASTIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-#define TEST int tt; cin >> tt; while(tt--) solve();
-
-
 
 bool visited[101][101];
-int graph[101][101], cnt[101][101];
+int graph[101][101];
+int n,m;
 int dx[] = {0,0,-1,1};
 int dy[] = {-1,1,0,0};
-int n,m;
-
-void bfs(){
+//상하좌우
+int cnt[101][101];
+void bfs(pair<int,int> target){
     visited[1][1] = true;
-    queue<pii>q;
-    q.push(pii(1,1));
+    queue<pair<int, int>>q;
+    q.push(pair<int, int>(1,1));
     cnt[1][1]++;
     while(!q.empty()){
-        pii p = q.front();
+        pair<int,int> p = q.front();
         q.pop();
         for(int i=0; i<4; i++){
             int x = p.first +dy[i];
             int y = p.second + dx[i];
-            if((x >= 1 &&  x<= n ) && ( y >= 1 && y <= m) && !visited[x][y] && graph[x][y] == 1 ){
+            if( ( x >= 1 && x<= n ) && ( y >= 1 && y <= m) && !visited[x][y] && graph[x][y] == 1 ){
                 visited[x][y] = true;
                 cnt[x][y] = cnt[p.first][p.second] + 1;
                 q.push(pair<int,int>(x,y));
@@ -37,20 +30,16 @@ void bfs(){
         }
     }
 }
-
-void solve(){
-    int i,j,k;
+int main(){
     cin >> n >> m;
-    for(i=1; i<=n; i++){
-        string s; cin >> s;
-        for(j=1; j<=m; j++){
-            if(s[j-1] == '1') graph[i][j]++;
+    int num;
+    for(int i=1; i<= n; i++){
+        for(int j=1; j<=m; j++){
+            scanf("%1d",&num);
+            graph[i][j] = num;
         }
     }
-    bfs();
+    bfs(pair<int, int>(n,m));
+
     cout << cnt[n][m];
-}
-int main(){
-    FASTIO
-    solve();
 }
