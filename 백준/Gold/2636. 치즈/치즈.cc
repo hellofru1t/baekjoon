8d6b51cf6bf649096f4b99a3444cab68c42575a8
hellofru1t <114278007+hellofru1t@users.noreply.github.com>
2022-10-07 16:20:34 +0900

    [Gold IV] Title: 치즈, Time: 0 ms, Memory: 2264 KB -BaekjoonHub

--- 백준/Gold/2636. 치즈/치즈.cc
+++ 백준/Gold/2636. 치즈/치즈.cc
@@ -10,20 +10,19 @@ typedef pair<double, double> pdd;
 #define TEST int tt; cin >> tt; while(tt--) solve();
 
 
-
-
-
 int edge[105][105];
 bool visited[105][105];
 int dy[]= {0,0,-1,1};
 int dx[] = {1,-1,0,0};
-int n,m,cnt,cnt2;
+int n,m,cnt1,cnt2;
 
 vector<pii>v;
 
-
+bool ok(int x, int y){
+    return x && y && x <= n && y <= m && !visited[x][y];
+}
 void bfs(int x,int y){
-    visited[x][y] = 1;
+    visited[x][y] = true;
     if(edge[x][y]== 1){
         v.push_back({x,y});
         return;
@@ -31,38 +30,34 @@ void bfs(int x,int y){
     for(int i=0; i<4; i++){
         int nx = x + dx[i];
         int ny = y + dy[i];
-        if(nx<0 || nx>=n || ny<0 || ny>=m || visited[nx][ny])continue;
-        bfs(nx,ny);
+        if(ok(nx,ny)) bfs(nx,ny);
     }
 }
 
 void solve(){
     int i,j,k;
     cin >> n >> m;
-    for(i=0; i<n; i++){
-        for(j=0; j<m; j++){
+    for(i=1; i<=n; i++){
+        for(j=1; j<=m; j++){
             cin>>edge[i][j];
         }
     }
     while(1){
-        cnt2 =0;
         memset(visited,0,sizeof(visited));
+        cnt1++, cnt2 = 0; 
         v.clear();
-        bfs(0,0);
-        for(pii b : v){
-            cnt2++;
-            edge[b.first][b.second] = 0;
-        }
+        bfs(1,1);
+        for(pii b : v) cnt2++, edge[b.first][b.second] = 0;
         bool flag = false;
-        for(i=0; i<n; i++){
-            for( j=0; j<m; j++){
+        for(i=1; i<=n; i++){
+            for( j=1; j<=m; j++){
                 if(edge[i][j] != 0) flag = true;
             }
         }
-        cnt++;
+  
         if(!flag) break;
     }
-    cout<< cnt << '\n' << cnt2 << '\n';
+    cout<< cnt1 << '\n' << cnt2 << '\n';
 }
 int main(){
     FASTIO
